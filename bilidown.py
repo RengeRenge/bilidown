134310d1ef00fb786105bb034619078f7ca26937
RengeRenge <lidrkuft123@163.com>
2025-02-14 14:30:05 +0800

    bug: 修复部分视频 biliplus 服务无法获取信息的问题

--- bilidown.py
+++ bilidown.py
@@ -5,10 +5,21 @@ import sys
 import yt_dlp
 import requests
 import re
-import shutil
+from json import JSONDecodeError
 
+api_biliplus_view = 'https://www.biliplus.com/api/view'
+api_bilibili_view = 'https://api.bilibili.com/x/web-interface/view'
+
+api_headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+    "Referer": "https://www.bilibili.com/"
+}
+    
 video_base_url = 'https://www.bilibili.com/video/av'
 video_keys = ['av', 'AV', 'BV', 'bv']
+
+def get_video_url(aid, p):
+    return f"{video_base_url}{aid}?p={p}"
 
 # @refer https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/misc/bvid_desc.md
 class BilibiliCodec:
@@ -83,13 +94,65 @@ class PageInfo:
         self.title = title
         self.dmlink = dmlink
         self.aid = aid
-        self.videoUrl = f"{video_base_url}{aid}?p={p}"
+        self.videoUrl = get_video_url(aid, p)
 
 
-def getPageInfo(aid, name_prefix='', p=1, update=0) -> PageInfo:
-    info = get_av_info(aid=aid, update=update)
+def validate_filename(name):
+    r_str = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_name = re.sub(r_str, " ", name)  # 替换为下划线
+    return new_name
+
+
+def get_av_info(aid, update=0, biliplus=True):
+    if biliplus == True:
+        url = f"{api_biliplus_view}?id={aid}&update={update}"
+    else:
+        url = f"{api_bilibili_view}?aid={aid}"
+
+    json = {}
+    error = False
+    try:
+        response = requests.get(url=url, headers=api_headers)
+        response.raise_for_status()
+        json = response.json()
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP Error: ({response.status_code}): {http_err}")
+        error = True
+    except requests.exceptions.RequestException as req_err:
+        print(f"Request Error: {req_err}")
+        error = True
+    except JSONDecodeError as e:
+        print(f"JSON Error: {e}")
+        print("Response Content:", response.text[:200])
+        error = True
+    except Exception as unexpected_err:  # 捕获其他所有异常
+        print(f"Error: {unexpected_err.__class__.__name__} - {unexpected_err}")
+        error = True
+
+    if biliplus == True and (error or ('code' in json and json['code'] == -404)):
+        return get_av_info(aid=aid, update=update, biliplus=False)
+
+    if biliplus == True:
+        if 'v2_app_api' in json:
+            json = json['v2_app_api']
+    else:
+        json = json['data']
+
+    title = validate_filename(json['title'])
+    pages = None
+    videos = 1
+
+    if 'pages' in json:
+        pages = json['pages']
+    if 'videos' in json:
+        videos = json['videos']
+    return {'title': title, 'pages': pages, 'videos': videos}
+
+
+def getPageInfo(aid, name_prefix='', p=1, update=0, damu=False) -> PageInfo:
+    info = get_av_info(aid=aid, update=update, biliplus=damu)
     if info['pages'] is None:
-        info = get_av_info(aid=aid, update=1)
+        info = get_av_info(aid=aid, update=1, biliplus=damu)
 
     title = validate_filename(info['title'])
     pages = info['pages']
@@ -99,23 +162,44 @@ def getPageInfo(aid, name_prefix='', p=1, update=0) -> PageInfo:
 
     for page in pages:
         if page['page'] == p:
-            dmlink = page['dmlink']
+            if 'dmlink' in page:
+                dmlink = page['dmlink']
             part_name = page['part']
             break
 
-    if dmlink is None and update == 0:
+    if damu == True and dmlink is None and update == 0:
         return getPageInfo(aid=aid, name_prefix=name_prefix, p=p, update=1)
     if name_prefix and len(name_prefix):
         title = name_prefix + ' ' + title
     if len(pages) > 1:
-        title = f"{title} [P{p}][{part_name}]"
+        width = max(2, len(str(info['videos'])))
+        title = f"{title} [P{p:0{width}d}][{part_name}]"
     return PageInfo(aid, p, title, dmlink)
-    
-        
-def validate_filename(name):
-    r_str = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
-    new_name = re.sub(r_str, " ", name)  # 替换为下划线
-    return new_name
+
+
+def do_get_danmu_video(av_numbers, cookie_path, damu=False, video=False, output_path=None, name_prefix='', p=1):
+    for aid in av_numbers:
+        print(f"--------------------------------")
+        info = getPageInfo(aid=aid, name_prefix=name_prefix, p=p, update=1, damu=damu)
+        if damu:
+            if info.dmlink is not None:
+                print(f"Downloading danmuku xml av{aid}")
+                req = requests.get(url=info.dmlink, headers=api_headers)
+                try:
+                    file_path = os.path.join(output_path, info.title + '.xml')
+                    with open(file_path, "wb") as f:
+                        f.write(req.content)
+                    print(f"finished xml av{aid}")
+                except Exception as e:
+                    print(f"{e}\n")
+            else:
+                print(f"Error: dmlink not find for av{aid}")
+
+        if video:
+            video_path = os.path.join(output_path, info.title)
+            downer = ytdlp(info.videoUrl, video_path, cookie_path)
+            downer.download()
+        print(f"--------------------------------")
 
 
 def get_danmu_video(av_numbers, cookie_path, damu=False, video=False, output_path=None, name_prefix='', p=1):
@@ -128,48 +212,7 @@ def get_danmu_video(av_numbers, cookie_path, damu=False, video=False, output_pat
     try:
         do_get_danmu_video(av_numbers=av_numbers, cookie_path=cookie_path, damu=damu, video=video, output_path=base_path, name_prefix=name_prefix, p=p)
     except Exception as e:
-        print(f"download error: {e}\n")
-
-
-def do_get_danmu_video(av_numbers, cookie_path, damu=False, video=False, output_path=None, name_prefix='', p=1):
-    for av_number in av_numbers:
-        print(f"--------------------------------")
-        info = getPageInfo(aid=av_number, name_prefix=name_prefix, p=p, update=1)
-        if damu: 
-            if info.dmlink is not None:
-                print(f"downloading danmuku xml av{av_number}")
-                req = requests.get(url=info.dmlink)
-                try:
-                    file_path = os.path.join(output_path, info.title + '.xml')
-                    with open(file_path, "wb") as f:
-                        f.write(req.content)
-                    print(f"finished xml av{av_number}")
-                except Exception as e:
-                    print(f"{e}\n")
-            else:
-                print(f"error: dmlink not find for av{av_number}")
-
-        if video:
-            video_path = os.path.join(output_path, info.title)
-            downer = ytdlp(info.videoUrl, video_path, cookie_path)
-            downer.download()
-        print(f"--------------------------------")
-
-
-def get_av_info(aid, update=0):
-    api = 'https://www.biliplus.com/api/view?id={}&update={}'.format(aid, update)
-
-    req = requests.get(url=api)
-    json = req.json()
-
-    title = validate_filename(json['title'])
-    pages = None
-    if 'v2_app_api' in json:
-        pages = json['v2_app_api']
-        if 'pages' in pages:
-            pages = pages['pages']
-
-    return {'title': title, 'pages': pages}
+        print(f"Download error: {e}\n")
 
 
 if __name__ == '__main__':
